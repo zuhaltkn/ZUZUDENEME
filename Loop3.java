5e0998ae877f58303ebcf3182c9e6eeabd5f89bb
zuhaltkn <zuhaltkn01@gmail.com>
2022-01-22 00:57:30 -0600

    mahmut

---
+++ Loop3.java
@@ -0,0 +1,21 @@
+package ArifZuhal;
+
+public class Loop3 {
+    public static void main(String[] args) {
+
+
+
+
+
+        for(int i=1; i<=5; i++){
+
+            for(int k=5; k>=i; k--){
+
+                System.out.print(k);
+            }
+            System.out.println();
+
+        }
+    }
+}
+
